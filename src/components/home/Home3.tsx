cc8aa9415355282cfe114b889a1853d1ebc28700
JjungminLee <julie0964@naver.com>
2023-08-22 17:09:48 +0900

    feat:홈화면 퍼블리싱

--- src/components/home/Home3.tsx
+++ src/components/home/Home3.tsx
@@ -1,42 +1,52 @@
 import SwipeText from '@assets/home/swipe/text_guide_swipe.svg';
-import Card1 from '@assets/home/swipe/card1.svg';
-import Card2 from '@assets/home/swipe/card2.svg';
-import Card3 from '@assets/home/swipe/card3.svg';
-import Card4 from '@assets/home/swipe/card4.svg';
-import Card5 from '@assets/home/swipe/card5.svg';
-
+import Image from 'next/image';
 interface ISwipeList {
   index: number;
-  image: React.FC<React.SVGProps<SVGSVGElement>>;
+  image: string;
 }
 export const swipeList: ISwipeList[] = [
   {
     index: 0,
-    image: Card1,
+    image: '/assets/home/swipe/card1.svg',
   },
   {
     index: 1,
-    image: Card2,
+    image: '/assets/home/swipe/card2.svg',
   },
   {
     index: 2,
-    image: Card3,
+    image: '/assets/home/swipe/card3.svg',
   },
   {
     index: 3,
-    image: Card4,
+    image: '/assets/home/swipe/card4.svg',
   },
   {
     index: 4,
-    image: Card5,
+    image: '/assets/home/swipe/card5.svg',
   },
 ];
 const Home3 = () => {
-  const [current, setCurrent] = useState(swipeList[0].index);
-
   return (
     <div className="flex flex-col justify-center items-center mt-[100px]">
       <SwipeText />
+      <div className="carousel rounded-box">
+        <div className="carousel-item">
+          <Image src={swipeList[0].image} width="1200" height="338" alt="card1" />
+        </div>
+        <div className="carousel-item">
+          <Image src={swipeList[1].image} width="1200" height="338" alt="card2" />
+        </div>
+        <div className="carousel-item">
+          <Image src={swipeList[2].image} width="1200" height="338" alt="card3" />
+        </div>
+        <div className="carousel-item">
+          <Image src={swipeList[3].image} width="1200" height="338" alt="card4" />
+        </div>
+        <div className="carousel-item">
+          <Image src={swipeList[4].image} width="1200" height="338" alt="card5" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,12 +1,17 @@
 import Home1 from './Home1';
 import Home2 from './Home2';
 import Home3 from './Home3';
+import Home4 from './Home4';
+import Home5 from './Home5';
+
 const Home = () => {
   return (
     <div className="h-100% w-screen flex flex-col items-center justify-center first-letter:overflow-scroll">
       <Home1 />
       <Home2 />
       <Home3 />
+      <Home4 />
+      <Home5 />
     </div>
   );
 };
