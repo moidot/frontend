4487d5a092d3324e7c08944033dc0ddfef6fb968
JjungminLee <julie0964@naver.com>
2023-11-05 17:43:27 +0900

    fix:getServerSideProps로 새로고침 해결

--- src/pages/main/[id].tsx
+++ src/pages/main/[id].tsx
@@ -1,13 +1,25 @@
 import DefaultLayout from '@/components/common/layout/defaultLayout';
 import Main from '@/components/main';
-const MainPage = () => {
+import { GetServerSideProps } from 'next';
+
+const MainPage = (props) => {
+  console.log(props);
   return (
     <>
       <DefaultLayout>
-        <Main />
+        <Main id={props.id} />
       </DefaultLayout>
     </>
   );
 };
 
 export default MainPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.params?.id;
+  return {
+    props: {
+      id,
+    },
+  };
+};
