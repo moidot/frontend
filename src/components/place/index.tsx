8f2ae2dc04520b915c931205a64dddb4a2d4dbac
JjungminLee <julie0964@naver.com>
2023-11-09 23:04:37 +0900

    add:chip click 이벤트 구현

--- src/components/place/index.tsx
+++ src/components/place/index.tsx
@@ -9,26 +9,27 @@ import ChipLibraryOff from '@assets/chip/chip_library_off.svg';
 import ChipLibraryOn from '@assets/chip/chip_library_on.svg';
 import ChipRestaurantOff from '@assets/chip/chip_restaurant_off.svg';
 import ChipRestaurantOn from '@assets/chip/chip_restaurant_on.svg';
-import ChipRoomOn from '@assets/chip/chip_room_off.svg';
+import ChipRoomOn from '@assets/chip/chip_room_on.svg';
 import ChipRoomOff from '@assets/chip/chip_room_off.svg';
 import ChipStudyOff from '@assets/chip/chip_study_off.svg';
 import ChipStudyOn from '@assets/chip/chip_study_on.svg';
 
+const ChipList = {
+  CAFE: 'CAFE',
+  STUDY_CAFE: 'STUDY_CAFE',
+  RESTAURANT: 'RESTAURANT',
+  LIBRARY: 'LIBRARY',
+  STUDY_ROOM: 'STUDY_ROOM',
+} as const;
+
 const Place = () => {
   const [click, setClick] = useState(false);
   const [portalElement, setPortalElement] = useState<Element | null>(null);
-  const [cafe, setCafe] = useState(false);
-  const [studyCafe, setStudyCafe] = useState(false);
-  const [restaurant, setRestaurant] = useState(false);
-  const [library, setLibrary] = useState(false);
-  const [studyRoom, setStudyRoom] = useState(false);
+  const [chip, setChip] = useState<keyof typeof ChipList>(ChipList.LIBRARY);
 
-  const onCafeClick = () => {
-    setCafe(!cafe);
-    setStudyCafe(false);
-    setRestaurant(false);
-    setLibrary(false);
-    setStudyRoom(false);
+  const onChipClick = (chip: keyof typeof ChipList) => {
+    console.log(chip);
+    setChip(chip);
   };
 
   useEffect(() => {
@@ -45,19 +46,51 @@ const Place = () => {
         <div className="flex flex-col ">
           <div className="font-Pretendard text-black text-h3 font-bold pt-[44px]">모여서 여기로 가는거 어때요?</div>
           <div className="flex flex-row gap-12 pt-[12px]">
-            {cafe ? (
-              <div onClick={onCafeClick}>
+            {chip == ChipList.CAFE ? (
+              <div>
                 <ChipCafeOn />
               </div>
             ) : (
-              <div onClick={onCafeClick}>
+              <div onClick={() => onChipClick(ChipList.CAFE)}>
                 <ChipCafeOff />
               </div>
             )}
-            {library ? <ChipLibraryOn /> : <ChipLibraryOff />}
-            {studyCafe ? <ChipStudyOn /> : <ChipStudyOff />}
-            {studyRoom ? <ChipRoomOn /> : <ChipRoomOff />}
-            {restaurant ? <ChipRestaurantOn /> : <ChipRestaurantOff />}
+            {chip == ChipList.LIBRARY ? (
+              <div>
+                <ChipLibraryOn />
+              </div>
+            ) : (
+              <div onClick={() => onChipClick(ChipList.LIBRARY)}>
+                <ChipLibraryOff />
+              </div>
+            )}
+            {chip == ChipList.STUDY_CAFE ? (
+              <div>
+                <ChipStudyOn />
+              </div>
+            ) : (
+              <div onClick={() => onChipClick(ChipList.STUDY_CAFE)}>
+                <ChipStudyOff />
+              </div>
+            )}
+            {chip == ChipList.STUDY_ROOM ? (
+              <div>
+                <ChipRoomOn />
+              </div>
+            ) : (
+              <div onClick={() => onChipClick(ChipList.STUDY_ROOM)}>
+                <ChipRoomOff />
+              </div>
+            )}
+            {chip == ChipList.RESTAURANT ? (
+              <div>
+                <ChipRestaurantOn />
+              </div>
+            ) : (
+              <div onClick={() => onChipClick(ChipList.RESTAURANT)}>
+                <ChipRestaurantOff />
+              </div>
+            )}
           </div>
           <div className="grid grid-cols-2 w-[100%] p-5 gap-8">
             <div onClick={onClick}>
