ffe8d42fbf08a13178def401446ba5a186b8a885
goeun208 <qop341@gmail.com>
2024-01-04 14:41:05 +0900

    Feat: 추천 장소 캐러셀 기능

--- src/components/main/Recommendation.tsx
+++ src/components/main/Recommendation.tsx
@@ -1,16 +1,27 @@
 import { GetGroupBestRegionListRes } from '@/types/SpaceType';
 import RecommendationItem from './RecommendationItem';
+import { useState } from 'react';
 
 const Recommendation = (props: GetGroupBestRegionListRes) => {
   const data = props.data;
+  const [slideIndex, setSlideIndex] = useState<number>(0);
+
   return (
-    <div>
-      <div className="carousel w-full">
+    <div className="mx-auto -mt-[84px] z-10 overflow-hidden w-[100vw]">
+      <div className="flex justify-start overflow-hidden">
         {data.map((item) => (
           <div
             key={data.indexOf(item)}
             id={data.indexOf(item).toString()}
-            className="carousel-item relative w-full  flex justify-center items-center">
+            className="mr-5"
+            onClick={() => {
+              setSlideIndex(data.indexOf(item));
+            }}
+            style={{
+              marginLeft: data.indexOf(item) === 0 ? '350px' : '',
+              transform: `translateX(${-100 * slideIndex}%)`,
+              transition: 'all 0.5s ease-in-out',
+            }}>
             <RecommendationItem
               userId={item.userId}
               name={item.name}
