6e354562a97fbddb8570dd9786ef3091a6c84908
goeun208 <80875784+goeun208@users.noreply.github.com>
2024-01-04 14:42:17 +0900

    Feat: 메인 추천 지역 캐러셀 (#48)
    * Feat: 투표 시작 전, 투표 생성 페이지 퍼블리싱
    
    * feat: 투표 상세 페이지 지도 마커 표시
    
    * feat: 투표 타임피커 제작
    
    * Feat: 투표 조회, 시작 API  연동 /
    Fix: 지도 마커 코드 수정, 날짜 포맷 코드 수정
    
    * Feat: 장소별 투표한 모임원 조회, 투표 진행바
    
    * feat:vote select 수정
    
    * Fix: vote filename 수정
    
    * Feat: 다시 투표하기 기능 추가, Fix: 투표 설정 시간 출력 오류 수정
    
    * Fix : 내 정보 수정 코드 변경, Feat: 익명투표, 투표 종료 시 투표 버튼 숨기기
    
    * Feat: 투표 종료 후 결과 확인
    
    * Feat: 추천 장소 캐러셀 기능

--- src/components/common/button/share/index.tsx
+++ src/components/common/button/share/index.tsx
@@ -1,22 +1,11 @@
-import Copy from '@assets/participate/icon_copy.svg';
-import KakaoTalk from '@assets/participate/icon_kakao_talk.svg';
+import UrlButton from '../url';
 
 const ShareButton = () => {
   return (
     <>
-      <div className="w-[555px] bg-bg_orange rounded-2xl text-center mx-auto mt-[30px] mb-[48px] p-[15px]">
+      <div className="w-[555px] font-Pretendard bg-bg_orange rounded-2xl text-center mx-auto mt-[30px] mb-[48px] p-[15px]">
         <div className="text-main_orange text-b1 font-bold mb-[15px]">모임원을 초대해보세요!</div>
-        <div className="flex w-[440px] justify-between items-center text-b3 text-font_black mx-auto">
-          <div className="flex items-center cursor-pointer text-b2">
-            URL 복사하기
-            <Copy className="ml-2" />
-          </div>
-          <div className="w-[1px] h-[26px] bg-bg_light_gray"></div>
-          <div className="flex items-center cursor-pointer text-b2">
-            카카오톡 공유하기
-            <KakaoTalk className="ml-2" />
-          </div>
-        </div>
+        <UrlButton />
       </div>
     </>
   );
