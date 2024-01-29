b6c22589774d3ebce242647783e3e02eb143e711
JjungminLee <julie0964@naver.com>
2024-01-29 14:54:05 +0900

    fix: 실서버 카카오 로그인

--- src/components/common/button/kakao/KakaoButton.tsx
+++ src/components/common/button/kakao/KakaoButton.tsx
@@ -3,8 +3,8 @@ import { useRouter } from 'next/router';
 
 const KakaoLoginButton = () => {
   const router = useRouter();
-  const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_APT_KEY;
-  const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+  const REST_API_KEY = 'da98b670c28171c6bae91e1f6fa272c3';
+  const REDIRECT_URI = 'https://moidot.vercel.app/login/kakao';
   const LINK = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   function kakaoLogin() {
