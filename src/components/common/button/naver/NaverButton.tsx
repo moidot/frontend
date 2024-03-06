04072deb4f44379d6db54514cce674f5a074141a
goeun208 <80875784+goeun208@users.noreply.github.com>
2024-03-06 20:27:54 +0900

    Fix: Naver url 변경 (#87)
    * Fix: 카카오톡 팀명 추가
    
    * Chore: 복사하기 버튼 css 수정
    
    * FIX: Naver Redirect URL 변경

--- src/components/common/button/naver/NaverButton.tsx
+++ src/components/common/button/naver/NaverButton.tsx
@@ -5,7 +5,7 @@ const NaverLoginButton = () => {
   const router = useRouter();
   const CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
   const REDIRECT_URI = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI;
-  const LINK = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=${'test'}&redirect_uri=${REDIRECT_URI}`;
+  const LINK = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI}`;
 
   function naverLogin() {
     router.push(LINK);
