ea6fb9023d0a1043831c0fdf89f493aa303dc473
goeun208 <qop341@gmail.com>
2024-03-08 01:01:57 +0900

    Fix: 장소조율 코드 변경

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -1,6 +1,5 @@
 // import BackButtonBar from '@/components/common/backButtonBar';
 import LoadingImg from '@assets/loading/icon_loading.svg';
-import LoadGif from '@assets/loading/gif_loading.svg';
 
 export default function LoadingPage() {
   // const onClickBack = () => {
@@ -8,13 +7,10 @@ export default function LoadingPage() {
   // };
 
   return (
-    <div className="font-Pretendard text-center">
-      <div className="text-h3 font-bold pt-40">잠시만 기다려주세요!</div>
+    <div className="font-Pretendard text-center py-40">
+      <div className="text-h3 font-bold">잠시만 기다려주세요!</div>
       <div className="text-b2 pt-2 pb-24 text-main_orange">우리가 만나는 지점, 어디일까요?</div>
       <LoadingImg className="mx-auto" />
-      <div className="mx-auto my-16 w-[48px] h-[48px] object-cover">
-        <LoadGif />
-      </div>
     </div>
   );
 }
