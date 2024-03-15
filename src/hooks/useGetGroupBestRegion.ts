5720cef31c393c99fc2f0e5ad19577397844e92d
goeun208 <qop341@gmail.com>
2024-03-16 00:44:47 +0900

    Fix: 새로고침 데이터 변경 안되는 문제 수정

--- src/hooks/useGetGroupBestRegion.ts
+++ src/hooks/useGetGroupBestRegion.ts
@@ -2,7 +2,5 @@ import { useQuery } from '@tanstack/react-query';
 import { GetGroupBestRegionListRes } from '@/types/SpaceType';
 import { getGroupBestRegion } from '@/apis/getGroupBestRegion';
 export const useGetGroupBestRegion = (groupId: number) => {
-  return useQuery<GetGroupBestRegionListRes>(['useGetGroupBestRegion'], () => getGroupBestRegion(groupId), {
-    staleTime: 300 * 1000,
-  });
+  return useQuery<GetGroupBestRegionListRes>(['useGetGroupBestRegion'], () => getGroupBestRegion(groupId));
 };
