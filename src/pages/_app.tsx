2d0d24bbd19bb0000dc1902e5684fb4e4b95d86d
goeun208 <qop341@gmail.com>
2024-03-30 00:32:10 +0900

    Refactor: SEO 개선을 위한 title, meta 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app';
 import '../../src/output.css';
 import { CookiesProvider } from 'react-cookie';
 import '../../src/index.css';
+import Head from 'next/head';
 declare global {
   interface Window {
     Kakao: any;
@@ -27,6 +28,11 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <CookiesProvider>
+          <Head>
+            <title>모이닷</title>
+            <meta name="keywords" content="중간위치 찾기" />
+            <meta name="description" content="모두를 위한 모임 통합 관리 서비스, 모이닷" />
+          </Head>
           <Component {...pageProps} />
           <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit}></Script>
           <Script src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js"></Script>
