944636b3d36cf046b251f5ff93b6387d3204ccfa
goeun208 <qop341@gmail.com>
2024-05-24 04:01:40 +0900

    Feat: google analytics 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Script from 'next/script';
 import type { AppProps } from 'next/app';
 import '../../src/output.css';
 import { CookiesProvider } from 'react-cookie';
 import '../../src/index.css';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+
 declare global {
   interface Window {
     Kakao: any;
@@ -24,6 +27,18 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }
 
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
@@ -32,7 +47,24 @@ export default function App({ Component, pageProps }: AppProps) {
             <title>모이닷</title>
             <meta name="keywords" content="중간위치 찾기" />
             <meta name="description" content="모두를 위한 모임 통합 관리 서비스, 모이닷" />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            />
           </Head>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
           <Component {...pageProps} />
           <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit}></Script>
           <Script src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js"></Script>
