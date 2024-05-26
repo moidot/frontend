3fcf3cfcb5f44514039052b4c719e45291be9491
goeun208 <qop341@gmail.com>
2024-05-27 04:06:46 +0900

    fix: prettier error

--- src/components/common/button/share/index.tsx
+++ src/components/common/button/share/index.tsx
@@ -6,7 +6,7 @@ const ShareButton = ({ teamname }: any) => {
   return (
     <>
       <div className="font-Pretendard w-[80vw] desktop:w-[555px] bg-bg_orange rounded-2xl text-center mx-auto mt-[30px] mb-[48px] p-[15px]">
-      <div className="text-main_orange text-mobile_b3 desktop:text-b1 font-bold mb-[15px]">
+        <div className="text-main_orange text-mobile_b3 desktop:text-b1 font-bold mb-[15px]">
           모임원을 초대해보세요!
         </div>
         <UrlButton pathname={locationUrl?.asPath} teamname={teamname} />
