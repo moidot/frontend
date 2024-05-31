6e141f13bbc98532adcfcc035838d7a5796d2cb7
goeun208 <qop341@gmail.com>
2024-06-01 00:47:15 +0900

    Style: 출발 위치 깃발 아이콘 크기 조정

--- src/components/spaceCreate/spaceCreatLocationModal/failSearchLocation.tsx
+++ src/components/spaceCreate/spaceCreatLocationModal/failSearchLocation.tsx
@@ -3,8 +3,8 @@ import EmptyPlaceImg from '@assets/map/empty_place.svg';
 const FailSearchLocation = () => {
   return (
     <div className="font-Pretendard py-10 mx-auto text-center">
-      <div className="text-font_black text-b2 mb-10">앗, 위치를 발견할 수 없어요!</div>
-      <div className="my-5">
+      <div className="text-font_black text-b2">앗, 위치를 발견할 수 없어요!</div>
+      <div className="my-5 mx-auto w-[300px]">
         <EmptyPlaceImg />
       </div>
       <div className="text-font_gray">
