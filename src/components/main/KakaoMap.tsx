d657dd3779c6899bd9b43e770faafc908a999cc7
goeun208 <qop341@gmail.com>
2024-06-13 23:08:47 +0900

    Feat: 푸터 링크 추가 외 2건

--- src/components/main/KakaoMap.tsx
+++ src/components/main/KakaoMap.tsx
@@ -19,7 +19,7 @@ const KakaoMap = ({ lng, lat, user, otherUser }: KakaoMapProps) => {
     const mapScript = document.createElement('script');
 
     mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JS_KEY}&autoload=false`;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JS_KEY}&autoload=false&libraries=services`;
 
     document.head.appendChild(mapScript);
 
