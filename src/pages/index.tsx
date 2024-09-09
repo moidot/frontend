46428a413965896c3031948e189a6d24dea873c9
goeun208 <qop341@gmail.com>
2024-09-09 17:26:21 +0900

    Feat: 서버 중지 시 띄울 이미지 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,24 @@
-import HomeLayout from '@/components/common/layout/homeLayout';
-//import LoginAfter from '@/components/home/LoginAfter';
-import LoginBefore from '@/components/home/LoginBefore';
-import api from '@/services/TokenService';
-import UserHomePage from './user';
+// import HomeLayout from '@/components/common/layout/homeLayout';
+// import LoginAfter from '@/components/home/LoginAfter';
+// import LoginBefore from '@/components/home/LoginBefore';
+// import api from '@/services/TokenService';
+// import UserHomePage from './user';
+import RenewalPage from '@/renewal';
 
 const HomePage = () => {
-  const token = api.getToken();
-  const userId = api.getId();
-  console.log('t', token);
+  // const token = api.getToken();
+  // const userId = api.getId();
+  // console.log('t', token);
   return (
     <>
-      {token && userId ? (
+      <RenewalPage />
+      {/* {token && userId ? (
         <UserHomePage />
       ) : (
         <HomeLayout>
           <LoginBefore />
         </HomeLayout>
-      )}
+      )} */}
     </>
   );
 };
